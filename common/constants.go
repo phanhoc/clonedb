18b8232e76fa7ff0144c4b83832a4a4246051130
phanhoc <phanhocit@gmail.com>
2018-09-16 21:59:47 +0700

    edit gitignore

--- common/constants.go
+++ common/constants.go
@@ -8,6 +8,11 @@ const (
 )
 
 const (
+	DATABASE_TYPE = "mysql"
+	DATABASE_CONN = "user:password@/dbname?charset=utf8&parseTime=True&loc=Local"
+)
+
+const (
 	PATH_MAIN_IMAGES string = "./datatest/images"
 	SERVER                  = "https://www.sunfrog.com/search/?cId=0&cName=&search=shirt"
 )
